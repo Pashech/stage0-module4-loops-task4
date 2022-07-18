af6824ff254567cbf149894cce0ec893cbd5abe4
Pavel Setsko <Pashech555@gmail.com>
2022-07-18 14:16:42 +0300

    done

--- src/main/java/school/mjc/stage0/loops/task4/SkippingThird.java
+++ src/main/java/school/mjc/stage0/loops/task4/SkippingThird.java
@@ -3,5 +3,21 @@ package school.mjc.stage0.loops.task4;
 public class SkippingThird {
     public void printUntilButThird(int lastPrinted) {
 
+
+
+        int count = 0;
+
+        for (int i = 1; i <= lastPrinted; i++){
+            if(lastPrinted <= 0){
+                System.out.println("");
+                return;
+            }
+            count++;
+            if(count == 3){
+                count = 0;
+                continue;
+            }
+            System.out.println(i);
+        }
     }
 }
--- src/main/java/school/mjc/stage0/loops/task4/TwoRangesSum.java
+++ src/main/java/school/mjc/stage0/loops/task4/TwoRangesSum.java
@@ -3,5 +3,27 @@ package school.mjc.stage0.loops.task4;
 public class TwoRangesSum {
     public void printSumOfTwoRanges(int numberToSkip, int lastInRow) {
 
+        if(numberToSkip > lastInRow){
+            System.out.println("number to skip is bugger then the last");
+            return;
+        }
+
+        if (lastInRow < 0){
+            System.out.println("last number in row is negative");
+            return;
+        }
+
+        int sum = 0;
+        int skippedSum = 0;
+
+        for (int i = 0; i <= numberToSkip; i++){
+            skippedSum += i;
+        }
+
+        for(int i = 0; i <= lastInRow; i++){
+            sum += i;
+        }
+        sum = sum - skippedSum;
+        System.out.println("skipped sum is " + skippedSum + "\n" + "counted sum is " + sum);
     }
 }
